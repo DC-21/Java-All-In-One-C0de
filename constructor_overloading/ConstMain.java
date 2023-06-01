f33498b824709dead6f0bfb0d43ace3ecdcc9b3e
DC-21 <cholahkuboko@gmail.com>
2023-06-01 15:33:21 +0200

    first commit

--- constructor_overloading/ConstMain.java
+++ constructor_overloading/ConstMain.java
@@ -1,25 +1,33 @@
-class rectangle{
+class Rectangle{
     int length;
     int breadth;
 
-    rectangle(){
+    Rectangle(){
         length = 0;
         breadth = 0;
     }
-    rectangle(int side){
+    Rectangle(int side){
         length = side;
         breadth = side;
     }
-    rectangle(int len, int brd){
+    Rectangle(int len, int brd){
         length = len;
         breadth = brd;
     }
-    int calculateArea{
+    int calculateArea () {
         return length * breadth;
     }
 }
 
 public class ConstMain{
+    public static void main(String[] args){
+        Rectangle rectangle1 = new Rectangle();
+        Rectangle rectangle2 = new Rectangle(3);
+        Rectangle rectangle3 = new Rectangle(4,5);
 
+        System.out.println("area of rectangle1 is:" + rectangle1.calculateArea());
+        System.out.println("area of rectangle2 is:" + rectangle2.calculateArea());
+        System.out.println("area of rectangle3 is:" + rectangle3.calculateArea());
+    }
 
 }
