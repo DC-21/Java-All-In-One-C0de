af2307ce4e76c863d0cd346052c9398d17166360
DC-21 <cholahkuboko@gmail.com>
2023-06-01 15:36:16 +0200

    first commit

--- palindrome/Palindrome.java
+++ palindrome/Palindrome.java
@@ -1,19 +1,5 @@
 class Palindrome{
     public static void main(String[] args){
-       String input = "radar";
-       boolean isPalindrome= true;
-       for (int i = 0; i <input.length() /2; i++){
-        if (input.charAt(i) != input.charAt((input.length()) -i -1)){
-            isPalindrome= false;
-            break;
-        }
-       } 
-       if(isPalindrome){
-            System.out.println(input+" is palindrome");
-        
-       }
-       else {
-        System.out.println(input+" is not palindrome");
-       }
+        String input = "radar";
     }
 }
