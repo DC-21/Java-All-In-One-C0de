5ce027ee6753927b5fcfab73f383910b96ed2918
DC-21 <cholahkuboko@gmail.com>
2023-06-01 15:39:29 +0200

    first commit

--- palindrome/Palindrome.java
+++ palindrome/Palindrome.java
@@ -3,5 +3,11 @@ class Palindrome{
         String input = "radar";
 
         boolean isPalindrome = true;
+
+        for(int i = 0; i < input.length()/2; i++){
+            if(input.charAt(i) != input.charAt(input.length()-i-1)){
+                isPalindrome = false;
+            }
+        }
     }
 }
